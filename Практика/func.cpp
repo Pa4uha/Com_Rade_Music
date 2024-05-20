70c8ea8c2b668e900d2c5f3c468e4a777a38d93b
com_Rade <83885020+Cmrade@users.noreply.github.com>
2024-05-20 22:30:04 +0300

    Add files via upload

--- Практика/func.cpp
+++ Практика/func.cpp
@@ -11,9 +11,9 @@ static vector<mus> Mass_mus;
 static set<int> Mass_fuu;
 
 void menu() {
-	Mass_mus.push_back(mus("Белые розы.mp3", "Ласковый май", 2));
-	Mass_mus.push_back(mus("Поговори со мной ольга.mp3", "Ночные снайперы", 1));
-	Mass_mus.push_back(mus("Орбит без сахара.mp3", "Сплин", 1));
+	Mass_mus.push_back(mus("Белые розы.mp3", "Ласковый май", 2));                     //
+	Mass_mus.push_back(mus("Поговори со мной ольга.mp3", "Ночные снайперы", 1));      //
+	Mass_mus.push_back(mus("Орбит без сахара.mp3", "Сплин", 1));                      //
 
 	AudioDevicePtr device = OpenDevice();
 	OutputStreamPtr sound = OpenSound(device, Mass_mus[0].get_name_song(), true);
@@ -21,8 +21,9 @@ void menu() {
 	int i = 0;
 	int k = 0;
 	char ch = '/';
-	cout << "1 - следующий трек \n 2 - предыдущий трек \n 3 - пауза \n 4 - плей \n 5 - дизлайк" << endl;
+	cout << "1 - следующий трек \n 2 - предыдущий трек \n 3 - пауза \n 4 - плей \n 5 - дизлайк \n 6 - поставить трек на повтор" << endl;
 	while (ch != 27) {
+		sound->setRepeat(false);
 		ch = _getch();
 		switch (ch) {
 		case '1':
@@ -40,17 +41,32 @@ void menu() {
 			}
 			sound = OpenSound(device, Mass_mus[i].get_name_song(), true);
 			sound->play();
+			cout << "Сейчас играет: " << Mass_mus[i].get_name_executor() << " - " << Mass_mus[i].get_name_song() << endl;
 			break;
 		case '2':
 			i--;
+			i = (i - 1) < 0 ? Mass_mus.size() - 1 : i - 1;
+			k = i;
+			while (Mass_fuu.count(Mass_mus[i].get_genre())) {
+				i--;
+				if (i < 0) {
+					i = Mass_mus.size() - 1;
+				}
+				if (i == k) {
+					cout << "В БД нет музыки, которая бы подходила под Ваши вкусовые предпочтения :(" << endl;
+					exit(1);
+				}
+			}
 			sound = OpenSound(device, Mass_mus[i].get_name_song(), true);
 			sound->play();
+			cout << "Сейчас играет: " << Mass_mus[i].get_name_executor() << " - " << Mass_mus[i].get_name_song() << endl;
 			break;
 		case '3':
 			sound->stop();
 			break;
 		case '4':
 			sound->play();
+			cout << "Сейчас играет: " << Mass_mus[i].get_name_executor() << " - " << Mass_mus[i].get_name_song() << endl;
 			break;
 		case '5':
 			Mass_fuu.insert(Mass_mus[i].get_genre());
@@ -68,6 +84,11 @@ void menu() {
 			}
 			sound = OpenSound(device, Mass_mus[i].get_name_song(), true);
 			sound->play();
+			cout << "Сейчас играет: " << Mass_mus[i].get_name_executor() << " - " << Mass_mus[i].get_name_song() << endl;
+			break;
+		case '6':
+			sound->setRepeat(true);
+			cout << "Трек стоит на повторе" << endl;
 			break;
 		case 27:
 			break;
