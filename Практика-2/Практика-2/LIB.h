514d29ba9e34db505337ff3b0bc05bb62e40982d
com_Rade <83885020+Cmrade@users.noreply.github.com>
2024-05-24 17:51:07 +0300

    Add files via upload
    Подвязка класса к интерфейсу
    Упрощение кода ивентов нажатий любых клавиш
    - Подвязка БД

---
+++ Практика-2/Практика-2/LIB.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <sstream>
+
+#include <vector>
+#include <set>
+
+#include "conio.h"
+#include "windows.h"
+#include "audiere.h"
+
+
+
+//using namespace audiere;
